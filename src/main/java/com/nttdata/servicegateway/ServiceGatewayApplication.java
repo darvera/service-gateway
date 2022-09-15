436a53ed65ce692855a5e8e57fb1df06a8c39dbe
darvera <darvera@protonmail.com>
2022-09-14 19:06:43 -0500

    update

--- src/main/java/com/nttdata/servicegateway/ServiceGatewayApplication.java
+++ src/main/java/com/nttdata/servicegateway/ServiceGatewayApplication.java
@@ -3,9 +3,11 @@ package com.nttdata.servicegateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@RibbonClient(name = "apiservice", configuration = RibbonConfig.class)
 public class ServiceGatewayApplication {
 
 	public static void main(String[] args) {
